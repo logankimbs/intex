855816a8ad763183f3ed82dd09d69fc8594a35f3
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-12-02 11:16:06 -0700

    added prescribers

--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import indexPageView
+from .views import prescribersPageView
+from .views import viewPrescriberPageView
+from .views import editPrescriberPageView
+from .views import createPrescriberPageView
 
 urlpatterns = [
-    path('', indexPageView, name='index')
+    path('', indexPageView, name='index'),
+    path('prescribers/', prescribersPageView, name='prescribers'),
+    path('viewprescriber/', viewPrescriberPageView, name='viewprescriber'),
+    path('editprescriber/', editPrescriberPageView, name='editprescriber'),
+    path('createprescriber/', createPrescriberPageView, name='createprescriber'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 
 def indexPageView(requests):
     return render(requests, 'portal/index.html')
+
+
+# this page displays all prescribers in a table
+def prescribersPageView(requests):
+    return render(requests, 'portal/prescribers.html')
+
+
+# this page shows a detailed view of a specific prescriber
+def viewPrescriberPageView(requests):
+    return render(requests, 'portal/viewprescriber.html')
+
+
+# this page allows user to edit a specific prescriber
+def editPrescriberPageView(requests):
+    return render(requests, 'portal/editprescriber.html')
+
+
+# this page allows user to create a prescriber
+def createPrescriberPageView(requests):
+    return render(requests, 'portal/createprescriber.html')
