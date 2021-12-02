16005615daf1909415322793f1b4ec019d319b2d
kyliefromm <90372645+kyliefromm@users.noreply.github.com>
2021-12-02 13:25:33 -0700

    created about and index page

--- portal/views.py
+++ portal/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+# this page displays our home page
 def indexPageView(requests):
     return render(requests, 'portal/index.html')
 
+# this page displays extra info about the opioid epidemic and prescribers of opioids. 
+def aboutPageView(requests):
+    return render(requests, 'portal/about.html')
+ 
 
 # this page displays all prescribers in a table
 def prescribersPageView(requests):
@@ -24,3 +28,9 @@ def editPrescriberPageView(requests):
 # this page allows user to create a prescriber
 def createPrescriberPageView(requests):
     return render(requests, 'portal/createprescriber.html')
+
+def drugsPageView(requests):
+    return render(requests, 'portal/drugs.html')
+
+def viewdrugPageView(requests):
+    return render(requests, 'portal/viewdrug.html')
