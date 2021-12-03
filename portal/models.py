82d373c69d2d45827083b85ce23c94f8ebd365be
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-12-03 11:55:08 -0700

    added states models

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    state = models.CharField(max_length=14)
+    stateabbrev = models.CharField(max_length=2)  # (PK)
+    population = models.IntegerField(default=0)
+    deaths = models.IntegerField(default=0)
+
+    def __str__(self):
+        return (self.stateabbrev)
